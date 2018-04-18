cb33c64f32dc1d7f5490982c17db66d79eda5099
Conor Lorsung <cglorsung@gmail.com>
2018-04-18 16:41:50 -0500

    Deletions and familiarization with pointers

--- src/Main.c
+++ src/Main.c
@@ -1,9 +1,28 @@
 #include "MainConfig.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <CL/cl.h>
 
 int hello() {
-    printf("Hello, world!");
+    printf("Hello, world!\n");
+    int x = 0, y = 1;
+    int *z;
+
+    z = &x;
+    printf("VALOF z = &x: %d | Expecting integer memory address\n", z);
+
+    y = *z;
+    printf("VALOF y = *z: %d | Expecting 0\n", y);
+
+    x = (int) z;
+    printf("VALOF x = z : %d | Expecting integer memory address\n", x);
+
+    *z = 3;
+    printf("VALOF *z = 3: x=%d ; y=%d ; z = %d\n", x, y, z);
+
+    //Print expected values for last assignment
+    printf("Expecting x, y, z = 3, 0, intMemAddr\n");
+
     return 0;
 }
 
